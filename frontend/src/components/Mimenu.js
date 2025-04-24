30f5202bd8cd6da2b7bb81791906ca07e3d88b41
MathiusZamora <mathiuszamora2@gmail.com>
2025-04-24 14:07:15 -0600

    main menu and background changed

--- frontend/src/components/Mimenu.js
+++ frontend/src/components/Mimenu.js
@@ -1,32 +1,44 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const MainMenu = () => {
-  const navigate = useNavigate();
+  const [isOverlayActive, setIsOverlayActive] = useState(false);
+
+  const handleOverlayToggle = () => {
+    setIsOverlayActive(!isOverlayActive);
+  };
 
   return (
-    <div className="container">
-      <h1>Generador de Documentos de Técnica</h1>
-      <div className="menu">
-        <button
-          className="menu-btn"
-          onClick={() => navigate('/solicitud-abastecimiento')}
-        >
-          Solicitud de Abastecimiento Servicio/Reparación
-        </button>
-        <button
-          className="menu-btn"
-          onClick={() => navigate('/control-horas-extras')}
-        >
-          Control de Horas Extras
-        </button>
-        <button
-          className="menu-btn"
-          onClick={() => navigate('/acta-entrega')}
-        >
-          Acta de Entrega
-        </button>
+    <div>
+      <div className={`overlay-navigation ${isOverlayActive ? 'overlay-active' : ''}`}>
+        <nav role="navigation">
+          <ul>
+            <li className={isOverlayActive ? 'slide-in-nav-item' : 'slide-in-nav-item-reverse'}>
+              <Link to="/solicitud-abastecimiento" data-content="Realzar Solicitud">
+                Solicitud de Abastecimiento
+              </Link>
+            </li>
+            <li className={isOverlayActive ? 'slide-in-nav-item-delay-1' : 'slide-in-nav-item-delay-1-reverse'}>
+              <Link to="/control-horas-extras" data-content="Realizar gestion de Horas Extras">
+                Control de Horas Extras
+              </Link>
+            </li>
+            <li className={isOverlayActive ? 'slide-in-nav-item-delay-2' : 'slide-in-nav-item-delay-2-reverse'}>
+              <Link to="/acta-entrega" data-content="Realizar Acta de Entrega">
+                Acta de Entrega
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
+
+      <section className="home">
+        <div className="open-overlay" onClick={handleOverlayToggle}>
+          <span className={`bar-top ${isOverlayActive ? 'animate-top-bar' : 'animate-out-top-bar'}`}></span>
+          <span className={`bar-middle ${isOverlayActive ? 'animate-middle-bar' : 'animate-out-middle-bar'}`}></span>
+          <span className={`bar-bottom ${isOverlayActive ? 'animate-bottom-bar' : 'animate-out-bottom-bar'}`}></span>
+        </div>
+      </section>
     </div>
   );
 };
